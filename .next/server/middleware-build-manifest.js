52ab9aea7a5693537b0344ae0a0b0d0849e8cac9
Md. Mehedi Hassan <mh.web.mehedihassan@gmail.com>
2024-11-30 13:06:47 +0600

    NE-001: Update gitignore file

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,33 +1,26 @@
-(self.__BUILD_MANIFEST = {
-  polyfillFiles: ['static/chunks/polyfills-42372ed130431b0a.js'],
-  devFiles: [],
+self.__BUILD_MANIFEST = {
+  polyfillFiles: ['static/chunks/polyfills.js'],
+  devFiles: ['static/chunks/react-refresh.js'],
   ampDevFiles: [],
   lowPriorityFiles: [],
-  rootMainFiles: [
-    'static/chunks/webpack-a05f68e3d168c82d.js',
-    'static/chunks/4bd1b696-80bcaf75e1b4285e.js',
-    'static/chunks/517-d083b552e04dead1.js',
-    'static/chunks/main-app-2aa870d623909818.js',
-  ],
+  rootMainFiles: ['static/chunks/webpack.js', 'static/chunks/main-app.js'],
   rootMainFilesTree: {},
   pages: {
     '/_app': [
-      'static/chunks/webpack-a05f68e3d168c82d.js',
-      'static/chunks/framework-0d635b52335dc518.js',
-      'static/chunks/main-4084c69bfdc260b1.js',
-      'static/chunks/pages/_app-d23763e3e6c904ff.js',
+      'static/chunks/webpack.js',
+      'static/chunks/main.js',
+      'static/chunks/pages/_app.js',
     ],
     '/_error': [
-      'static/chunks/webpack-a05f68e3d168c82d.js',
-      'static/chunks/framework-0d635b52335dc518.js',
-      'static/chunks/main-4084c69bfdc260b1.js',
-      'static/chunks/pages/_error-9b7125ad1a1e68fa.js',
+      'static/chunks/webpack.js',
+      'static/chunks/main.js',
+      'static/chunks/pages/_error.js',
     ],
   },
   ampFirstPages: [],
-}),
-  (self.__BUILD_MANIFEST.lowPriorityFiles = [
-    '/static/' + process.env.__NEXT_BUILD_ID + '/_buildManifest.js',
-    ,
-    '/static/' + process.env.__NEXT_BUILD_ID + '/_ssgManifest.js',
-  ]);
+};
+self.__BUILD_MANIFEST.lowPriorityFiles = [
+  '/static/' + process.env.__NEXT_BUILD_ID + '/_buildManifest.js',
+  ,
+  '/static/' + process.env.__NEXT_BUILD_ID + '/_ssgManifest.js',
+];
