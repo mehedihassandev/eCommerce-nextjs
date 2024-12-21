88982a8e579807ee9dcd6d16f8e0f0093a0873aa
amit-360 <amit@swordfishtech.co.uk>
2024-12-21 16:29:50 +0600

    NE-030: API with middleware

---
+++ app/api/(auth)/users/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from 'next/server';
+import connect from '@/lib/db';
+import User from '@/lib/modals/user';
+
+export const GET = async () => {
+  try {
+    await connect();
+    const users = await User.find();
+    return new NextResponse(JSON.stringify(users), { status: 200 });
+  } catch (error) {
+    return new NextResponse('Error in fetching users' + error, { status: 500 });
+  }
+};
+
+export const POST = async (request: Request) => {
+  try {
+    const body = await request.json();
+
+    await connect();
+    const newUser = new User(body);
+    await newUser.save();
+
+    return new NextResponse(
+      JSON.stringify({ message: 'User is created', user: newUser }),
+      { status: 201 },
+    );
+  } catch (error) {
+    return new NextResponse(
+      JSON.stringify({
+        message: 'Error in creating user',
+        error,
+      }),
+      {
+        status: 500,
+      },
+    );
+  }
+};
---
+++ middlewares/api/authMiddleware.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const validateToken = (token: any) => {
+  const validToken = true;
+  if (!token || !validToken) {
+    return false;
+  }
+
+  return true;
+};
+
+export function authMiddleware(request: Request): any {
+  const token = request.headers.get('authorization')?.split(' ')[1];
+
+  return { isValid: validateToken(token) };
+}
