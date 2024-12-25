bebad4671e40121489638b01c02c35e15659bc0b
Md. Mehedi Hassan <mh.web.mehedihassan@gmail.com>
2024-12-25 19:37:49 +0600

    NE-041: Refactor cart model; remove IItemPrice interface and streamline imports for improved clarity

--- app/models/cart.ts
+++ app/models/cart.ts
@@ -1,19 +1,8 @@
-import { IPrice, IProduct } from './products';
+import { IProduct } from './products';
 
 export interface IAmount {
   value: number;
   unit: string;
-}
-
-export interface IItemPrice {
-  description?: string;
-  name?: string;
-  priceType?: string;
-  recurringChargePeriod?: number | string;
-  unitOfMeasure?: string;
-  price?: IPrice;
-  cart_item_price?: number;
-  cart_item_total_price?: number;
 }
 
 export interface ICartItem {
